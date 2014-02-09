58ce6266a670c8eb5d76b8bc6a83ca263a79c38f
Shichao An <shichao.an@nyu.edu>
2014-02-08 20:23:09 -0500

    Added astyle command

---
+++ pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Astyle
+astyle -r *.c *.h
+
+# Make clean
+find . -mindepth 1 -path "./.git" -prune -o -type d -exec make -C {} clean \;
+
+# Remove executales
+find . -mindepth 1 -path "./.git" -prune -o -type f -perm +111 -exec rm {} \;
