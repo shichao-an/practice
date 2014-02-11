6ae2cf2ee55bcf3ec169b27ac53bfd83e5c12419
Shichao An <shichao.an@nyu.edu>
2014-02-11 18:49:59 -0500

    Updated test case using math.factorial

--- misc/permutation.py
+++ misc/permutation.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 
 
@@ -25,11 +26,16 @@ class TestPermutation(unittest.TestCase):
         self.assertEqual(len(perms1), 1)
         assert '' in perms1
         s2 = 'abcd'
+        f = math.factorial(len(s2))
         perms2 = get_permutations(s2)
-        self.assertEqual(len(perms2), 24)
+        self.assertEqual(len(perms2), f)
         current = perms2[0]
-        for i in range(1, 24):
+        assert len(current) == len(s2)
+        for i in range(1, f):
             _current = perms2[1]
+            assert len(_current) == len(s2)
+            for c in s2:
+                assert c in _current
             assert _current != current
             _current = current
 
