5e74f78cf139d1e231f7554698616fa2f363d142
Shichao An <shichao.an@nyu.edu>
2014-02-19 19:31:14 -0500

    Added get_single_integer

--- misc/integer.py
+++ misc/integer.py
@@ -1,7 +1,20 @@
+def get_single_integer(integers):
+    """
+    Given a list of `integers`, every element appears twice except for one.
+    Return that single one.
+    """
+    if not integers:
+        return None
+    p = integers[0]
+    for i in range(1, len(integers)):
+        p = p ^ integers[i]
+    return p
+
+
 def reverse_integer(number):
     t = 0
     if number > 0:
-        positive = True 
+        positive = True
     else:
         positive = False
         number = -number
@@ -16,3 +29,5 @@ if __name__ == '__main__':
     assert reverse_integer(976) == 679
     assert reverse_integer(500) == 5
     assert reverse_integer(0) == 0
+    a = [2, 5, 5, 8, 9, 6, 2, 3, 9, 3, 6]
+    print(get_single_integer(a))
