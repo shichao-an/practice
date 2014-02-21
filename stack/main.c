4392bf745c8cf0b926abac018ec99696f584edc7
Shichao An <shichao.an@nyu.edu>
2014-02-21 02:25:32 -0500

    Added macro stack_empty

--- stack/main.c
+++ stack/main.c
@@ -8,11 +8,14 @@ int main()
     Stack *stack = stack_create();
     stack_push(stack, 1);
     stack_push(stack, 2);
+    int t = stack_top(stack);
+    assert(t == 2);
+    assert(!stack_empty(stack));
     int a = stack_pop(stack);
     int b = stack_pop(stack);
     assert(a == 2);
     assert(b == 1);
-    assert(stack->size == 0);
+    assert(stack_empty(stack));
     stack_destroy(stack);
     return 0;
 }
--- stack/stack.h
+++ stack/stack.h
@@ -7,6 +7,7 @@ typedef List Stack;
 
 #define stack_init list_init
 #define stack_destroy list_destroy
+#define stack_empty(stack) ((stack)->size == 0 ? 1 : 0)
 
 Stack *stack_create();
 void stack_push(Stack *stack, const int data);
