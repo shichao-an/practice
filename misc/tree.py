71b264bcb4e01c1bc1731e3fb702a333c6c1cac6
Shichao An <shichao.an@nyu.edu>
2014-02-20 23:44:38 -0500

    Added unique_num_bst with recursive and DP solutions

--- misc/tree.py
+++ misc/tree.py
@@ -27,6 +27,33 @@ def is_same_tree(a, b):
             is_same_tree(a.right, b.right))
 
 
+def unique_num_bst_recursive(n):
+    """
+    Given n, how many structurally unique BST's (binary search trees) that
+    store values 1...n?
+
+    """
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    s = 0
+    for i in range(n):
+        s += unique_num_bst_recursive(i) * unique_num_bst_recursive(n - i - 1)
+    return s
+
+
+def unique_num_bst(n):
+    # Initialize extra array `t`
+    t = [0] * (n + 1)
+    t[0] = 1
+    for i in range(1, n + 1):
+        t[i] = 0
+        for j in range(i):
+            t[i] += t[j] * t[i - j - 1]
+    return t[n]
+
+
 if __name__ == '__main__':
     root = new_tree_node(1)
     root.left = new_tree_node(2)
@@ -35,3 +62,7 @@ if __name__ == '__main__':
     print(max_tree_depth(root))
     print(is_same_tree(root, root.left))
     print(is_same_tree(root, root))
+    assert unique_num_bst(0) == 1
+    assert unique_num_bst(1) == 1
+    assert unique_num_bst(2) == 2
+    assert unique_num_bst(3) == 5
