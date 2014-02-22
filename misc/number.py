2d5e93a626cfa880855190a26f24345792f363a2
Shichao An <shichao.an@nyu.edu>
2014-02-21 21:36:33 -0500

    Minor fixed

--- misc/number.py
+++ misc/number.py
@@ -152,7 +152,7 @@ def get_max_profit(prices):
     if not prices:
         return 0
     max_profit = 0
-    min_price = prices[0] 
+    min_price = prices[0]
     for i, p in enumerate(prices, 0):
         max_profit = max(max_profit, (p - min_price))
         min_price = min(min_price, p)
