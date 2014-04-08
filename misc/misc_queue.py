90263ecbb062449ae59008c97b9f216fbc80a3d7
Shichao An <shichao.an@nyu.edu>
2014-04-08 18:56:03 -0400

    Added queue-stack

--- misc/misc_queue.py
+++ misc/misc_queue.py
@@ -17,8 +17,32 @@ class StackQueue(object):
         return self.stack2.pop()
 
 
-if __name__ == '__main__':
+class QueueStack(object):
+    """A Stack implemented with two queues"""
+    def __init__(self):
+        self.queue1 = []
+        self.queue2 = []
 
+    def push(self, data):
+        if self.queue1:
+            self.queue1.append(data)
+        else:
+            self.queue2.append(data)
+
+    def pop(self):
+        # q is non-empty queue; e is empty queue
+        if self.queue1:
+            q = self.queue1
+            e = self.queue2
+        else:
+            q = self.queue2
+            e = self.queue1
+        while len(q) != 1:
+            e.append(q.pop(0))
+        return q.pop(0)
+
+
+def test_stack_queue():
     sq = StackQueue()
     sq.enqueue(1)
     sq.enqueue(2)
@@ -30,3 +54,16 @@ if __name__ == '__main__':
     print(sq.dequeue())
     sq.enqueue(6)
     print(sq.dequeue())
+
+
+def test_queue_stack():
+    qs = QueueStack()
+    qs.push(1)
+    qs.push(2)
+    print(qs.pop())
+    qs.push(3)
+    print(qs.pop())
+    print(qs.pop())
+
+if __name__ == '__main__':
+    test_queue_stack()
