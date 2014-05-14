c9ec46949ec28679f1e1a0124226929e4cd8b429
Shichao An <shichao.an@nyu.edu>
2014-05-14 01:46:44 -0400

    Fixed heap

--- misc/misc_heap.py
+++ misc/misc_heap.py
@@ -12,10 +12,26 @@ def get_largest_k_numbers(nums, k):
                 heapq.heappush(h, c)
             else:
                 heapq.heappush(h, min_c)
-    return h
+    return [heapq.heappop(h) for i in range(len(h))]
+
+
+def get_smallest_k_numbers(nums, k):
+    h = []
+    for c in nums:
+        print h
+        if len(h) < k:
+            heapq.heappush(h, (-c, c))
+        else:
+            key, max_c = heapq.heappop(h)
+            if c < max_c:
+                heapq.heappush(h, (-c, c))
+            else:
+                heapq.heappush(h, (key, max_c))
+    return [heapq.heappop(h)[1] for i in range(len(h))]
 
 
 if __name__ == '__main__':
-    r1 = [8, 3, 4, 5, 1, 6, 7, 9, 2]
+    r1 = [8, 3, 4, 5, 11, 6, 7, 9, 2]
     k1 = 4
     print get_largest_k_numbers(r1, k1)
+    print get_smallest_k_numbers(r1, k1)
