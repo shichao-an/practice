d1c5beb2b5df2f60204ca47e43986e40bb54ca6b
Shichao An <shichao.an@nyu.edu>
2014-05-17 15:57:18 -0400

    Added generic

--- misc/misc_select.py
+++ misc/misc_select.py
@@ -9,6 +9,17 @@ def partition(a, left, right):
     return j
 
 
+def partition2(a, left, right):
+    pivot = right
+    j = left
+    for i in range(left, right):
+        if a[i] > a[pivot]:
+            a[i], a[j] = a[j], a[i]
+            j += 1
+    a[j], a[pivot] = a[pivot], a[j]
+    return j
+
+
 def quickselect(a, left, right, k):
     if left <= right:
         p = partition(a, left, right)
@@ -45,6 +56,34 @@ def get_k_smallest(a, k):
             left = p + 1
 
 
+def get_k_largest(a, k):
+    left = 0
+    right = len(a) - 1
+    while left <= right:
+        p = partition2(a, left, right)
+        if p == k - 1:
+            return a[:k]  # a[0...k - 1]
+        elif p > k - 1:
+            right = p - 1
+        else:
+            left = p + 1
+
+
+def get_k_largest2(a, k):
+    left = 0
+    right = len(a) - 1
+    # The k-th largest is (n + 1 - k)-th smallest
+    k = len(a) + 1 - k
+    while left <= right:
+        p = partition(a, left, right)
+        if p == k - 1:
+            return a[k - 1:]  # a[k - 1...n - 1]
+        elif p > k - 1:
+            right = p - 1
+        else:
+            left = p + 1
+
+
 def test_quickselect(a, k):
     left = 0
     right = len(a) - 1
@@ -74,3 +113,5 @@ if __name__ == '__main__':
     test_quickselect2(a3, 3)
     test_quickselect2(a4, 5)
     print get_k_smallest(a4, 5)
+    print get_k_largest(a4, 2)
+    print get_k_largest2(a4, 2)
