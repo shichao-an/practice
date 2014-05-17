42ea7463c3c690cce1f4fc49e810a828803f0642
Shichao An <shichao.an@nyu.edu>
2014-05-17 15:34:22 -0400

    Added quickselect

---
+++ misc/misc_select.py
@@ -0,0 +1,76 @@
+def partition(a, left, right):
+    pivot = right
+    j = left
+    for i in range(left, right):
+        if a[i] <= a[pivot]:
+            a[i], a[j] = a[j], a[i]
+            j += 1
+    a[j], a[pivot] = a[pivot], a[j]
+    return j
+
+
+def quickselect(a, left, right, k):
+    if left <= right:
+        p = partition(a, left, right)
+        if p == k:
+            return a[k]
+        elif p > k:
+            return quickselect(a, left, p - 1, k)
+        else:
+            return quickselect(a, p + 1, right, k)
+
+
+def quickselect2(a, left, right, k):
+    """Iterative"""
+    while left <= right:
+        p = partition(a, left, right)
+        if p == k:
+            return a[k]
+        elif p > k:
+            right = p - 1
+        else:
+            left = p + 1
+
+
+def get_k_smallest(a, k):
+    left = 0
+    right = len(a) - 1
+    while left <= right:
+        p = partition(a, left, right)
+        if p == k - 1:
+            return a[:k]
+        elif p > k - 1:
+            right = p - 1
+        else:
+            left = p + 1
+
+
+def test_quickselect(a, k):
+    left = 0
+    right = len(a) - 1
+    print quickselect(a, left, right, k - 1)
+
+
+def test_quickselect2(a, k):
+    left = 0
+    right = len(a) - 1
+    print quickselect2(a, left, right, k - 1)
+
+
+if __name__ == '__main__':
+    a1 = [1]
+    a2 = [1, 2]
+    a3 = [1, 2, 3, 4, 5, 6]
+    a4 = [1, 2, 3, 4, 5, 6, 7, 8]
+    import random
+    random.shuffle(a4)
+    print a4
+
+    test_quickselect(a1, 1)
+    test_quickselect(a2, 1)
+    test_quickselect(a3, 3)
+    test_quickselect2(a1, 1)
+    test_quickselect2(a2, 1)
+    test_quickselect2(a3, 3)
+    test_quickselect2(a4, 5)
+    print get_k_smallest(a4, 5)
